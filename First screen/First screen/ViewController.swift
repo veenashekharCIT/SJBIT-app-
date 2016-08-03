46e56eca8a3ee3efa508fc099bcaad81455607e5
student <student@Veena-2.local>
2016-08-03 10:34:51 +0530

    Master branch created

---
+++ First screen/First screen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  First screen
+//
+//  Created by student on 03/08/16.
+//  Copyright © 2016 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
